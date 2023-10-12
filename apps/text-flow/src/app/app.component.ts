50dae6c1a3b76593156ab869b37ea7f1e7754bba
br1ttan <brittanvladislav@gmail.com>
2023-10-12 18:11:16 +0300

    Initial commit

---
+++ apps/text-flow/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'text-flow-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'text-flow';
+}
